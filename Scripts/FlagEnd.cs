dc9c2c13a5ef14be5c0e17785e2e1c287cf9e42b
DaVexx <62082196+Dabvexx@users.noreply.github.com>
2021-08-11 14:31:40 -0400

    Updated flag logic

---
+++ Scripts/FlagEnd.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlagEnd : EndLevel
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Touch flag");
+        }
+        //find out which segment they touched, the higher up the more  points
+
+        FindIntersect();
+    }
+
+    private void FindIntersect()
+    {
+        updateRayCastOrigins();
+        CalculateRaySpacing();
+
+        GameObject flag = transform.parent.gameObject.transform.GetChild(1).GetChild(1).gameObject;
+        IEnumerator coroutine = MoveFlag(flag);
+
+        //shoot ray upwards and find where it intersects with the players hitbox
+
+        float rayLength = GetComponent<BoxCollider2D>().size.y;
+
+        Vector2 rayOrigin = raycastOrigins.bottomLeft;
+        rayOrigin.y -= skinWidth;
+        rayOrigin.x -= skinWidth;
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up, rayLength, collisionMask);
+
+        Debug.DrawRay(rayOrigin, Vector2.up * rayLength, Color.red);
+
+        if (hit)
+        {
+            Debug.Log("hit");
+
+            StartCoroutine(coroutine);
+            FindIndex(hit);
+            return;
+        }
+
+        rayOrigin = raycastOrigins.bottomRight;
+        rayOrigin.y -= skinWidth;
+        rayOrigin.x += skinWidth;
+        hit = Physics2D.Raycast(rayOrigin, Vector2.up, rayLength, collisionMask);
+
+        Debug.DrawRay(rayOrigin, Vector2.up * rayLength, Color.red);
+
+        if (hit)
+        {
+            Debug.Log("hit");
+
+            FindIndex(hit);
+            StartCoroutine(coroutine);
+            return;
+        }
+    }
+
+    private IEnumerator MoveFlag(GameObject flag)
+    {
+        flag.transform.position -= Vector3.up * .2f;
+        if (flag.transform.position.Equals(Vector2.zero))
+        {
+            yield break;
+        }
+        yield return new WaitForEndOfFrame();
+    }
+
+    private void FindIndex(RaycastHit2D hit)
+    {
+        int distance = (int)(hit.fraction * 100);
+        int index;
+
+        Debug.Log(distance);
+
+        if (hit.fraction >= 92)
+        {
+            index = 6;
+        }
+        else
+        {
+            index = 0;
+        }
+
+        //Find index with height on flag
+        PointHandler.CalculatePointsGained(index);
+        return;
+    }
+}
---
+++ Scripts/PointHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PointHandler
+{
+    static int points = 0;
+
+    public static void CalculatePointsGained(int index)
+    {
+        int[] PointValues = { 200, 500, 1000, 2000, 4000, 8000, 10000 };
+        points += PointValues[index];
+    }
+}
